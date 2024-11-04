ed3061d7ec9e8a5e0a8bb8ae6a513281904fff61
Muhammad Jamil <jamil138.amin@gmail.com>
2024-11-04 14:48:13 +0500

    react

---
+++ Lab 9 - React JS/my-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          Hello World!
+        </h1>
+        <p>
+          My First React Project
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
